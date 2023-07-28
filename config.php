48e33ed1fba54b2fc1a4fe50c3e724dfdf220579
Andrea Charvière <65179805+AndreaCodinLife@users.noreply.github.com>
2023-07-29 00:04:29 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+// Check if the user selected a language
+if (isset($_GET['lang'])) {
+    $selectedLang = $_GET['lang'];
+
+    // Validate the selected language (as mentioned in Step 2)
+
+    $_SESSION['lang'] = $selectedLang;
+}
+
+// Load the language file based on the selected language or use a default language
+if (isset($_SESSION['lang'])) {
+    $langFile = 'lang_' . $_SESSION['lang'] . '.php';
+} else {
+    $langFile = 'lang_en.php'; // Default language is English
+}
+
+// Include the language file
+include($langFile);
+
+// Example of displaying multilingual content
+echo $lang['welcome']; // Output: Welcome
+echo $lang['hello'];   // Output: Hello
+echo $lang['goodbye']; // Output: Goodbye
+?>
